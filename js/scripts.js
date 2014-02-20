b884aeb89fa3350385632708e688a169239f6f0c
Atanasova <batanaso@engr-131-04.eastern.ewu.edu>
2014-02-20 14:33:08 -0800

    version weird

---
+++ js/scripts.js
@@ -0,0 +1,15 @@
+// Make Foundation Go!
+$(document).foundation();
+
+// Your Awesome Scripts!
+$(document).ready(function(){
+
+	var msg1 = 'Hello!';
+	var msg2 = 'Hola!';
+	var msg3 = 'Bonjour!';
+
+	$('h1').text(msg3);
+
+	console.log(msg3);
+
+}); 
