37692835c2e7b2f3344bec50e851b0f252b53c8e
Atanasova <batanaso@engr-131-04.eastern.ewu.edu>
2014-02-20 14:49:58 -0800

    yeaa

--- js/scripts.js
+++ js/scripts.js
@@ -4,12 +4,16 @@ $(document).foundation();
 // Your Awesome Scripts!
 $(document).ready(function(){
 
+//return 0 to 'limit'
+var limit = 2;
+var numRand = Math.floor(Math.random()*limit);
+	
 	var msg1 = 'Hello!';
 	var msg2 = 'Hola!';
 	var msg3 = 'Bonjour!';
 
-	$('h1').text(msg3);
+	$('h1').text(msg + numRand);
 
-	console.log(msg3);
+	console.log(msg + numRand);
 
 }); 
